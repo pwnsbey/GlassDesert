27509c2b332abd19bf140146e9c0cbb96e5e3ac7
pwnsbey <jake.ownsbey@gmail.com>
2019-04-15 12:07:06 -0600

    Initial commit

---
+++ app/src/main/java/com/example/glassdesert/OverviewActivity.java
@@ -0,0 +1,14 @@
+package com.example.glassdesert;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class OverviewActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_overview);
+    }
+}
